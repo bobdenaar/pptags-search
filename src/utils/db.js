ac6d1424797849d9d3b17d523a792d81402e155a
Robert de Naar <bob.de.naar.du.94@gmail.com>
2023-05-13 11:17:02 +0200

    adding dist

--- src/utils/db.js
+++
@@ -1,6 +0,0 @@
-import Dexie from "dexie";
-
-export const db = new Dexie("ppTags");
-db.version(1).stores({
-  tags: "id, name, category, description, numSaves, imageUrl, ownerUsername, liveData, featured",
-});
