62a5b8cf67256c9ddfe298445cd7f27bcec9112c
Robert de Naar <bob.de.naar.du.94@gmail.com>
2023-05-13 09:20:55 +0200

    fix images

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,8 +1,14 @@
+import sleepUrl from "../assets/sleep.gif";
+
 export function ErrorMessage() {
   return (
     <>
       <h2>An error occurred. We're working hard at fixing it.</h2>
-      <img className="dachshund" src="/sleep.gif" alt="A dachshund, sleeping" />
+      <img
+        className="dachshund"
+        src={sleepUrl}
+        alt="A dachshund, sleeping"
+      />
     </>
   );
 }
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,12 @@
+import loadingUrl from "../assets/loading.gif";
+
 export function Spinner() {
   return (
     <>
       <h2>Loading...</h2>
       <img
         className="dachshund"
-        src="/loading.gif"
+        src={loadingUrl}
         alt="A dachshund, going nowhere"
       />
     </>
